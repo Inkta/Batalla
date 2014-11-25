b3a56ade9bfeb0d9003ff4f0e4fdf047ca253330
Inkta <mquerluque@gmail.com>
2014-11-25 15:17:46 +0100

    Batalla Finalitzada

---
+++ Batalla/src/guerra/joc.java
@@ -0,0 +1,27 @@
+package guerra;
+
+import java.util.ArrayList;
+
+import acm.program.GraphicsProgram;
+
+public class joc extends GraphicsProgram {
+
+	public final void run() {
+	setSize(1200,800);
+	pause(100);
+	double finalpantalla = getWidth() - 50;
+	double[] extrems = {0,finalpantalla};
+	ArrayList<Soldat> exercit1 = new ArrayList<Soldat>();
+	ArrayList<Soldat> exercit2 = new ArrayList<Soldat>();
+	Camp guerra = new Camp(this, exercit1, exercit2);
+	General General = new General(this);
+	exercit1 = General.afegirSoldats(exercit1,"esponja.png", false,extrems);
+	exercit2 = General.afegirSoldats(exercit2,"patricio.png",true,extrems);
+
+	guerra.Guerra();
+	System.out.println("Final de la Batalla");
+
+	}
+
+
+}
